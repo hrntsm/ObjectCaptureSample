8fffef2a59e3648026c236992426251a8a85c47e
hrntsm <contact@hrntsm.com>
2021-12-04 13:51:00 +0900

    Update code

--- ObjectCaptureSample/main.swift
+++ ObjectCaptureSample/main.swift
@@ -10,71 +10,55 @@ import os
 import RealityKit
 import Metal
 
-run()
+// 入出力の設定
+let inputFolder = "/Users/hrntsm/Desktop/ObjectCaptureItems/FruitCakeSlice"
+let outputFilename = "/Users/hrntsm/Desktop/item.usdz"
 
-func run() {
-    let inputFolder = "/Users/hrntsm/Desktop/FruitCakeSlice"
-    
-    let inputFolderUrl = URL(fileURLWithPath: inputFolder, isDirectory: true)
-    var configure = PhotogrammetrySession.Configuration()
-    configure.sampleOrdering = PhotogrammetrySession.Configuration.SampleOrdering.unordered
-    configure.featureSensitivity =
-        PhotogrammetrySession.Configuration.FeatureSensitivity.normal
-    
-    var maybeSession: PhotogrammetrySession? = nil
+// Session の設定作成
+let inputFolderUrl = URL(fileURLWithPath: inputFolder, isDirectory: true)
+var configure = PhotogrammetrySession.Configuration()
+configure.sampleOrdering = PhotogrammetrySession.Configuration.SampleOrdering.unordered
+configure.featureSensitivity =
+    PhotogrammetrySession.Configuration.FeatureSensitivity.normal
+
+let session = try PhotogrammetrySession(
+    input: inputFolderUrl,
+    configuration: configure
+)
+
+// 動作実行時の状態出力の設定作成
+let waiter = Task {
     do {
-        maybeSession = try PhotogrammetrySession(
-            input: inputFolderUrl,
-            configuration: configure
-        )
-    } catch {
-        Foundation.exit(1)
-    }
-    
-    guard let session = maybeSession else {
-        Foundation.exit(1)
-    }
-    
-    let waiter = Task {
-        do {
-            for try await output in session.outputs {
-                switch output {
-                    case .processingComplete:
-                        print("complete")
-                        Foundation.exit(0)
-                    case .requestError(let request, let error):
-                        print("Request \(String(describing: request)) had an error: \(String(describing: error))")
-                @unknown default:
-                    print("Output: unhandled message")
-                }
+        for try await output in session.outputs {
+            switch output {
+                case .processingComplete:
+                    print("complete!!")
+                    Foundation.exit(0)
+            @unknown default:
+                print("Output: unhandled message")
             }
-        } catch {
-            print("Output: ERROR")
-            Foundation.exit(1)
         }
-    }
-    
-    // The compiler may deinitialize these objects since they may appear to be
-    // unused. This keeps them from being deallocated until they exit.
-    withExtendedLifetime((session, waiter)) {
-        // Run the main process call on the request, then enter the main run
-        // loop until you get the published completion event or error.
-        do {
-            let request = makeRequestFromArguments()
-            print("Using request: \(String(describing: request))")
-            try session.process(requests: [ request ])
-            // Enter the infinite loop dispatcher used to process asynchronous
-            // blocks on the main queue. You explicitly exit above to stop the loop.
-            RunLoop.main.run()
-        } catch {
-            print("Process got error: \(String(describing: error))")
-            Foundation.exit(1)
-        }
+    } catch {
+        print("Output: ERROR")
+        Foundation.exit(1)
     }
 }
 
+// 処理の実行
+withExtendedLifetime((session, waiter)) {
+    do {
+        let request = makeRequestFromArguments()
+        print("Using request: \(String(describing: request))")
+        try session.process(requests: [ request ])
+        RunLoop.main.run()
+    } catch {
+        print("Process got error: \(String(describing: error))")
+        Foundation.exit(1)
+    }
+}
+
+// 完成したファイルの出力先とファイル作成の詳細度を設定
 private func makeRequestFromArguments() -> PhotogrammetrySession.Request {
-    let outputFilename = "/Users/hrntsm/Desktop/item.usdz"
     let outputUrl = URL(fileURLWithPath: outputFilename)
     
     return PhotogrammetrySession.Request.modelFile(
